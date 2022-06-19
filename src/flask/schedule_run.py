4cd28cebcef1dbe328f8ceeb7993c45830e6a449
cccccc <ccccccminecraft62@gmail.com>
2022-06-19 11:50:11 +0900

    チアポ

--- src/flask/schedule_run.py
+++ src/flask/schedule_run.py
@@ -3,7 +3,7 @@ from time import sleep
 from MinecraftServerChecker import MinecraftServerChecker
 
 def task():
-    mc_checker = MinecraftServerChecker
+    mc_checker = MinecraftServerChecker()
     mc_checker.run()
 
 schedule.every(300).seconds.do(task)
