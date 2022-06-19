073eb742c426be5f6f72c53ef14022e1dabed7a3
cccccc <ccccccminecraft62@gmail.com>
2022-06-19 11:42:53 +0900

    fix register

--- src/flask/ServerRegister.py
+++ src/flask/ServerRegister.py
@@ -4,14 +4,7 @@ from RedisController import RedisController
 # サーバーリストに新規サーバーを登録するスクリプトです
 
 # テスト用データ
-data = {
-    "server_name": "Mohist Home Server",
-    "game": "minecraft",
-    "owner": "CCCCCC",
-    "address": "192.168.0.176:25566",
-    "players": "",
-    "ping": ""
-}
-
-redis_controller = RedisController()
-redis_controller.insert_server(data)
+class ServerRegister:
+    def regist_server(self, data):
+        redis_controller = RedisController()
+        redis_controller.insert_server(data)
